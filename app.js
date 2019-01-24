422bd8aae4936c003041f138b6d35f7f7ff745b9
jomorro <morrowjoe0@gmail.com>
2019-01-24 11:31:14 -0500

    finished files for app

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const publicFolderPath = path.join(__dirname, "public")
+const app = express();
+const publicFolderPath = path.join(__dirname, "public");
 
-app.use(express.json())
-app.use(express.static(publicFolderPath))
+app.use(express.json());
+app.use(express.static(publicFolderPath));
 
-const users = []
+const users = [];
 
 // add POST request listener here
-
+app.post("/api/user/", function(request, response) {
+  if (
+    users
+      .map(userObject => userObject.username)
+      .some(username => username == request.body.username)
+  ) {
+    response.status(409).send({ error: "username already in use" });
+  } else {
+    const user = request.body;
+    user.id = Math.floor(Math.random() * 1000000000);
+    users.push(user);
+    response.status(201).send(user);
+  }
+});
+app.get("/api/user/", function(request, response) {
+  response.send(users);
+});
 app.listen(3000);
--- public/index.js
+++ public/index.js
@@ -1,2 +1,44 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+const submissionSuccessDisplayNode = document.getElementById("submission-status"); 
+
+userCreateSubmitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const username = document.querySelector('input[name="username"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const zip_code = document.querySelector('input[name="zip-code"]').value;
+    const fav_color = document.querySelector('input[name="fav-color"]').value;
+    const User = {
+        username,
+        email,
+        zip_code,
+        fav_color
+    };
+
+    const jsonUser = JSON.stringify(User);
+
+    fetch('/api/user/', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json; charset=utf-8"
+        },
+        body: jsonUser
+    }).then(response => {
+        // console.log(response);
+        if (response.status == 201) {
+            console.log("successfully added");
+            const successText = document.createTextNode('Your account as been successfully submitted!');
+            submissionSuccessDisplayNode.innerHTML = "";
+            submissionSuccessDisplayNode.appendChild(successText);
+        } else if (response.status == 409) {
+            console.log('username already in use');
+            const failureText = document.createTextNode('That user name already in use.');
+            submissionSuccessDisplayNode.innerHTML = '';
+            submissionSuccessDisplayNode.appendChild(failureText);
+        }
+        return response.json();
+    })
+    .then(responseObject => {
+        console.log("response:\n", responseObject);
+    });
+});
